bac5c1729d98233c095120f3501b5357fb373754
Dilber Bilgin <dilberkrkt@gmail.com>
2026-02-28 00:18:21 +0000

    en

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //  Backend bağlantı ayarları
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Spring Boot adresi
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api', // Spring Boot adresi
   headers: {
     'Content-Type': 'application/json',
   },
